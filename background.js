dabf9a715e2b538353d15398ce5018d5482cb68a
ashlinaronin <ashlin.aronin@pollinate.com>
2016-03-29 15:19:56 -0700

    Save [empty] image to desktop

--- background.js
+++ background.js
@@ -1,3 +1,15 @@
+var bigBro = {
+    canvas: null,
+    context: null,
+    webcam: null,
+    dataURL: null,
+    blob: null
+}
+bigBro.canvas = document.querySelector('canvas#selfie-canvas');
+bigBro.context = bigBro.canvas.getContext('2d');
+bigBro.webcam = document.querySelector('video#webcam');
+
+
 chrome.browserAction.onClicked.addListener(function(tab) {
     console.log(tab.url + ' clicked me');
 
@@ -5,11 +17,70 @@ chrome.browserAction.onClicked.addListener(function(tab) {
         console.log(streamId);
 
         // then call GUM with streamId
+        setupWebcam(streamId);
     });
-
 });
 
-///An opaque string that can be passed to getUserMedia() API to generate media stream that corresponds
-//to the source selected by the user. If user didn't select any source (i.e. canceled the prompt)
-//then the callback is called with an empty streamId. The created streamId can be used only once and
-// expires after a few seconds when it is not used.
+
+function setupWebcam(streamId) {
+    if (!streamId) {
+        console.log('sorry no');
+        return;
+    }
+
+    navigator.webkitGetUserMedia({
+        audio: {
+            mandatory: {
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: streamId
+            }
+        },
+        video: {
+            mandatory: {
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: streamId,
+                maxWidth: screen.width,
+                maxHeight: screen.height
+            }
+        }
+    }, gotStream, getUserMediaError);
+}
+
+
+function gotStream(stream) {
+    console.log('received local stream', stream);
+    takePic(stream);
+}
+
+function getUserMediaError(error) {
+    console.log('navigator.webkitGetUserMedia() error: ', error);
+}
+
+function takePic(stream) {
+    bigBro.context.drawImage(bigBro.webcam, 0, 0);
+    bigBro.dataURL = bigBro.canvas.toDataURL('image/png');
+    dataURIToBlob(bigBro.dataURL, 'image/png', downloadScreenshot);
+}
+
+function downloadScreenshot(blob) {
+    var objUrl = URL.createObjectURL(blob);
+    chrome.downloads.download({url: objUrl}, function(downloadId) {
+        console.log("downloading, downloadId is:" + downloadId);
+    });
+}
+
+
+
+// from https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toBlob
+var dataURIToBlob = function(dataURI, mimetype, cb) {
+    var binStr = atob( dataURI.split(',')[1] );
+    var len = binStr.length;
+    var arr = new Uint8Array(len);
+
+    for (var i = 0; i < len; ++i) {
+        arr[i] = binStr.charCodeAt(i);
+    }
+
+    var blob = new Blob([arr], {type: mimetype});
+    return cb(blob); // invoke cb with response
+};
